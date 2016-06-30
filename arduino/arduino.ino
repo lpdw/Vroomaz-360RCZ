3aa74b47602cb8693fc442892610d184bd4e4263
Enguerran P <contact@epoulain.fr>
2016-07-01 00:44:57 +0200

    New setup

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -8,41 +8,84 @@
 #define ECHO_PIN2     9  // Arduino pin tied to echo pin on the ultrasonic sensor.
 
 #define TRIGGER_PIN3  4  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN3     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define ECHO_PIN3     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
 
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-SoftwareSerial espSerial(5, 6); // RX:5, TX:6
+#define DETECT_DISTANCE 80
+
 NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar3(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+//Line detection
+int lineSensorLeftPin = 7;  // This is our input pin
+int lineSensorRightPin = 6;  // This is our input pin
+int lineSensorLeft = HIGH;  // HIGH MEANS NO OBSTACLE
+int lineSensorRight = HIGH;  // HIGH MEANS NO OBSTACLE
+
+SoftwareSerial espSerial(2, 3); // RX, TX
 
 void setup() {
 
   Serial.begin(9600);
   espSerial.begin(9600);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  pinMode(lineSensorLeftPin, INPUT);
+  pinMode(lineSensorRightPin, INPUT);
+  pinMode(13, OUTPUT);
 
-  while (!Serial) {
+  /*while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
-  }
+  }*/
 
 }
 
 void loop() { // run over and over
-  if (sonar1.ping_cm() < 80 && sonar1.ping_cm() > 0) {
-    Serial.println("Sonar 1");
-    espSerial.println(0);
+
+  //Obstacle
+  if (sonar1.ping_cm() < DETECT_DISTANCE && sonar1.ping_cm() > 0) {
+    //Serial.println("Sonar 1");
+    espSerial.write(1);
+    digitalWrite(13, HIGH);
   }
-  if (sonar2.ping_cm() < 80 && sonar2.ping_cm() > 0) {
-    Serial.println("Sonar 2");
-    espSerial.println(0);
+  if (sonar2.ping_cm() < DETECT_DISTANCE && sonar2.ping_cm() > 0) {
+    //Serial.println("Sonar 2");
+    espSerial.write(2);
+    digitalWrite(13, HIGH);
   }
-  if (sonar3.ping_cm() < 80 && sonar3.ping_cm() > 0) {
-    Serial.println("Sonar 3");
-    espSerial.println(0);
+  if (sonar3.ping_cm() < DETECT_DISTANCE && sonar3.ping_cm() > 0) {
+    //Serial.println("Sonar 3");
+    espSerial.write(3);
+    digitalWrite(13, HIGH);
   }
-  delay(100);
+
+  //Line sensor
+  lineSensorLeft = digitalRead(lineSensorLeftPin);
+  lineSensorRight = digitalRead(lineSensorRightPin);
+  if (lineSensorRight == LOW && lineSensorLeft == HIGH)
+  {
+    //Serial.println("Ligne à droite - tourner à droite");
+    espSerial.write(6); //6 = right, as it is on a numpad
+  }
+  else if (lineSensorRight == LOW && lineSensorLeft == LOW)
+  {
+    //Serial.println("Ligne au millieu - aller tout droit");
+     espSerial.write(8); //8 = up, as it is on a numpad
+  }
+  else if (lineSensorRight == HIGH && lineSensorLeft == LOW)
+  {
+    //Serial.println("Ligne à gauche - tourner à gauche");
+    espSerial.write(4); //4 = left, as it is on a numpad
+  }
+  else
+  {
+    //Serial.println("Pas de ligne - aller tout droit");
+    espSerial.write(9); //9 = because can't write 0
+  }
+
+  delay(50);
+  digitalWrite(13, LOW);
+  delay(50);
+
 }
--- esp/esp.ino
+++ esp/esp.ino
@@ -3,7 +3,19 @@
 
 #include "pindef-nodemcu1.0.h"
 
-SoftwareSerial espSerial(SD3, SD2);  // RX, TX
+#define STATIC_IP false
+// Update these with values suitable for your network.
+//IPAddress ip(192,168,1,239);  //Node static IP 
+//IPAddress gateway(192,168,1,1);
+//IPAddress subnet(255,255,255,0);
+//172.24.1.122
+
+//172.16.13.216
+IPAddress ip(172,16,13,216);  //Node static IP 
+IPAddress gateway(172,16,13,254);
+IPAddress subnet(255,255,255,0);
+
+SoftwareSerial arduinoSerial(D3, D4);  // RX, TX
 WiFiServer server(80);
 
 int motor_speed;
@@ -13,18 +25,20 @@ int speed_turn = 1023;
 // last command, used to stop correctly
 String last_command = "start";
 
-int motor1_enablePin = D7; //pwm
-int motor1_in1Pin = D5;
-int motor1_in2Pin = D6;
+int motor1_enablePin = D5; //pwm
+int motor1_in1Pin = D0;
+int motor1_in2Pin = D1;
 
-int motor2_enablePin = D0; //pwm
+int motor2_enablePin = D6; //pwm
 int motor2_in1Pin = D2;
-int motor2_in2Pin = D1;
+int motor2_in2Pin = D7;
 
 int driveMode = 0; //0:man 1:auto
 
-const char* ssid = "LPDW-IOT";
-const char* password = "LPDWIOTROUTER2015";
+//const char* ssid = "LPDW-IOT";
+//const char* password = "LPDWIOTROUTER2015";
+//const char* ssid = "raspberrypi-vroomaz";
+//const char* password = "raspberrypi-vroomaz";
 
 void setup() {
 
@@ -40,25 +54,28 @@ void setup() {
   pinMode(motor2_enablePin, OUTPUT);
 
   Serial.begin(9600);
-  espSerial.begin(9600);
+  arduinoSerial.begin(9600);
 
   while (!Serial) {
-    ;
+    ; // wait for serial port to connect. Needed for native USB port only
   }
 
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
+  if (STATIC_IP == true) WiFi.config(ip, gateway, subnet);
 
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
     digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
     Serial.print(".");
+    delay(250);
   }
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("WiFi connected");;
 
   server.begin();
   Serial.println("Server started");
@@ -71,18 +88,21 @@ void setup() {
 
 void loop() {
 
-  // Check if a client has connected
+  // Check if a client is connected
   WiFiClient client = server.available();
   if (!client) {
     if (driveMode == 1) {
       start(speed_default);
-      if (espSerial.available()) {
-        Serial.write(espSerial.read());
+      if (arduinoSerial.available()) {
+        
 
-        if (espSerial.read() < 1) {
-          stopp(speed_default);
+        int arduinoSerialValue = arduinoSerial.read();
+
+        Serial.write(arduinoSerialValue);
+
+        if (arduinoSerialValue == 1 || arduinoSerialValue == 2 || arduinoSerialValue == 3) {
+          if (previousArduinoSerialValue != arduinoSerialValue) stopp(speed_default);
           left(speed_turn);
-          delay(250);
         }
       }
     }
@@ -101,6 +121,7 @@ void loop() {
 
   if (req.indexOf("/mode/man") != -1) {
     driveMode = 0;
+    stopp(speed_default);
   }
   else if (req.indexOf("/mode/auto") != -1) {
     driveMode = 1;
@@ -150,31 +171,39 @@ void loop() {
 void start(int motor_speed) {
   last_command = "start";
   analogWrite(motor1_enablePin, motor_speed);
-  digitalWrite(motor1_in1Pin, true);
-  digitalWrite(motor1_in2Pin, false);
+  digitalWrite(motor1_in1Pin, false);
+  digitalWrite(motor1_in2Pin, true);
   analogWrite(motor2_enablePin, motor_speed);
-  digitalWrite(motor2_in1Pin, true);
-  digitalWrite(motor2_in2Pin, false);
+  digitalWrite(motor2_in1Pin, false);
+  digitalWrite(motor2_in2Pin, true);
 }
 
 void stopp(int motor_speed) {
-  if (last_command == "left")
-  {
-    right(speed_turn); //TODO : replace by motor_speed
-    delay(100);
-    left(0);
-  }
-  else if (last_command == "right")
-  {
-    left(speed_turn); //TODO : replace by motor_speed
-    delay(100);
-    right(0);
-  }
-  else if (last_command == "back")
-  {
-    start(motor_speed);
-    delay(100);
-    back(0);  
+  if (driveMode == 0) {
+    if (last_command == "left")
+    {
+      right(speed_turn); //TODO : replace by motor_speed
+      delay(100);
+      left(0);
+    }
+    else if (last_command == "right")
+    {
+      left(speed_turn); //TODO : replace by motor_speed
+      delay(100);
+      right(0);
+    }
+    else if (last_command == "back")
+    {
+      start(motor_speed);
+      delay(100);
+      back(0);  
+    }
+    else
+    {
+      back(motor_speed);
+      delay(100);
+      start(0);
+    }
   }
   else
   {
@@ -187,29 +216,29 @@ void stopp(int motor_speed) {
 void back(int motor_speed) {
   last_command = "back";
   analogWrite(motor1_enablePin, motor_speed);
-  digitalWrite(motor1_in1Pin, false);
-  digitalWrite(motor1_in2Pin, true);
+  digitalWrite(motor1_in1Pin, true);
+  digitalWrite(motor1_in2Pin, false);
   analogWrite(motor2_enablePin, motor_speed);
-  digitalWrite(motor2_in1Pin, false);
-  digitalWrite(motor2_in2Pin, true);
+  digitalWrite(motor2_in1Pin, true);
+  digitalWrite(motor2_in2Pin, false);
 }
 
 void right(int motor_speed) {
   last_command = "right";
-  analogWrite(motor1_enablePin, motor_speed);
-  digitalWrite(motor1_in1Pin, true);
-  digitalWrite(motor1_in2Pin, false);
-  analogWrite(motor2_enablePin, motor_speed);
-  digitalWrite(motor2_in1Pin, false);
-  digitalWrite(motor2_in2Pin, true);
-}
-void left(int motor_speed) {
-  last_command = "left";
   analogWrite(motor1_enablePin, motor_speed);
   digitalWrite(motor1_in1Pin, false);
   digitalWrite(motor1_in2Pin, true);
   analogWrite(motor2_enablePin, motor_speed);
   digitalWrite(motor2_in1Pin, true);
   digitalWrite(motor2_in2Pin, false);
+}
+void left(int motor_speed) {
+  last_command = "left";
+  analogWrite(motor1_enablePin, motor_speed);
+  digitalWrite(motor1_in1Pin, true);
+  digitalWrite(motor1_in2Pin, false);
+  analogWrite(motor2_enablePin, motor_speed);
+  digitalWrite(motor2_in1Pin, false);
+  digitalWrite(motor2_in2Pin, true);
 }
 
